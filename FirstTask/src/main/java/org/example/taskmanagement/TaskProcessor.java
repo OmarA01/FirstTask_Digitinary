feca1d7f739d7a5796e310dd41cfdc0eaa1a8116
OmarAsaad <omar321asaad@gmail.com>
2024-07-20 03:27:10 +0300

    additional comments

--- FirstTask/src/main/java/org/example/taskmanagement/TaskProcessor.java
+++ FirstTask/src/main/java/org/example/taskmanagement/TaskProcessor.java
@@ -3,16 +3,33 @@ package org.example.taskmanagement;
 import java.util.*;
 import java.util.concurrent.*;
 
+/**
+ * TaskProcessor class to execute tasks in multiple threads
+ */
 public class TaskProcessor{
-
+    /**
+     * ExecuterService for working directly with threads
+     */
     private ExecutorService executerService;
+
+    /**
+     * lock object to provide a synchronization mechanism for the execution
+     */
     private final Object lock = new Object();
 
-
+    /**
+     * Constructor to initialize a new TaskProcessor
+     * @param ThreadPoolSize: number of threads needed in the pool
+     */
     public TaskProcessor(int ThreadPoolSize){
         this.executerService = Executors.newFixedThreadPool(ThreadPoolSize);
     }
 
+    /**
+     * method to execute a list of tasks using lock as a synchronization mechanism, and using
+     * thread.sleep() to simulate the processing time of a task
+     * @param tasks
+     */
     public void executeTasks(List<Task> tasks){
         for(Task task : tasks){
             executerService.execute(() -> { // I can make Task extend Thread and pass task here, but I wanted to use lambda somewhere :)
@@ -34,6 +51,10 @@ public class TaskProcessor{
 
     }
 
+    /**
+     * method to stop accepting new tasks and gracefully shut down the service by allowing
+     * currently running tasks to complete. (wait for 30 seconds)
+     */
     public void shutdown() {
         executerService.shutdown();
         try {
